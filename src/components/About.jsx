02c60ab845ac95a4447c43860656dec40454e43e
nethmalgunawardhana <nethmalgunawardhana@gmail.com>
2025-05-30 10:26:08 +0530

    feat: Update About component layout and background images for improved design

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,16 +5,16 @@ const About = () => {
       <img 
         src="/Vector.svg" 
         alt="Background Vector" 
-        className="absolute bottom-10 left-4 w-32 h-32 sm:w-40 sm:h-40 md:w-48 md:h-48 md:opacity-80 opacity-25 z-11"
+        className="absolute bottom-48 left-28 w-32 h-32 sm:w-40 sm:h-40 md:w-28 md:h-28 md:opacity-80 opacity-100 z-11000000"
       />
       
       {/* Background Vector - Top Left Corner */}
-      <img 
+      <img
         src="./about_Vector.png" 
         alt="Background Star" 
-        className="absolute bottom-36 left-2 w-16 h-12 sm:w-20 sm:h-20 md:w-40 md:h-[500px] opacity-100 z-20"
+        className="absolute bottom-24 left-0 w-16 h-12 sm:w-20 sm:h-20 md:w-44 md:h-[550px] opacity-80 z-0"
       />
-      
+
       {/* Terminal Window main container */}
       <div className="bg-[#0A2324] w-full max-w-4xl rounded-lg overflow-hidden relative z-10">
         <div className="bg-[#05383C] h-9 flex items-center justify-end px-3 sm:px-4"> 
@@ -26,18 +26,16 @@ const About = () => {
         </div>
 
         {/* Content Area */}
-        <div className="p-6 sm:p-8 md:p-10 text-slate-300">         
-           {/* Logo Section */}
+          <div className="px-12 sm:px-16 md:px-32 py-6 sm:py-8 md:py-14 text-justify text-slate-300">         
+             {/* Logo Section */}
           <div className="text-center mb-6 sm:mb-6">
             <img 
               src="/b1.png" 
               alt="Innovate with Ballerina Logo" 
               className="mx-auto max-w-65 sm:max-w-52 md:max-w-80 w-full h-auto"
             />
-          </div>
-
-          {/* Text Content Section */}
-          <p className="text-sm sm:text-base md:text-lg leading-relaxed sm:leading-loose">
+          </div>          {/* Text Content Section */}
+          <p className="text-xs sm:text-sm md:text-base leading-relaxed sm:leading-loose text-justify">
             Innovate with Ballerina is an idea hackathon organized by the <Bold>IEEE Student Branch</Bold> in collaboration with the <Bold>IEEE CS Student Branch Chapter</Bold> of <Bold>University of Moratuwa</Bold>, and proudly powered by <Bold>WSO2</Bold>. The competition is open to <Bold>university undergraduates</Bold> across Sri Lanka, where participants are challenged to integrate their innovative ideas using the <Bold>Ballerina programming language</Bold>. The journey features expert-led workshop guiding teams from concepts to implementation, and the <Bold>top 10 teams</Bold> will advanced to the finals which will be held at WSO2 premises.
           </p>
         </div>
