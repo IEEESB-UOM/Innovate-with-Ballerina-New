66a0d029a6c5a816ad07605dc20312bb097409b9
PulinduVR <pulinduv@gmail.com>
2025-06-02 14:54:22 +0530

    Timeline done

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -4,28 +4,42 @@ import '../stylesheets/timeline.css';
 const Timeline = () => {
   return (
     <div className='relative h-screen w-full snap-start bg-gradient-to-b from-[#0A2324] via-[#0A2324] to-[#0F262A] flex items-center justify-center text-4xl font-bold'>
-      {/* <div className="background-svg">
-                        <BackgroundLine/>
-                    </div> */}
-        <img src="/Ellipse 7.png" className="absolute left-[0px] top-[250px]"/>
-        <img src="/Ellipse 8.png" className="absolute left-[200px] top-[600px]"/>
-        <img src="/Ellipse 9.png" className="absolute right-[-30px] top-[450px]"/>
-        <div className='text-background-div1'>
-          <p className='text-background'>Stay Updated</p>
-        </div>
-        {/* <div className='text-background-div2'>
-          <p className='text-background'>Updated  </p>
-        </div> */}
-      <div className="absolute left-[185px] top-[180px]">
-        <TimeLineCard text={"Registrations Open"} month={"June"} day={"15"} />
-      </div>
-      <div className="absolute left-[885px] top-[387px]">
-        <TimeLineCard text={"Opening Registration"} month={"June"} day={"15"} />
-      </div>
-      <div className="absolute left-[435px] top-[575px]">
-        <TimeLineCard text={"Opening Registration"} month={"June"} day={"15"} />
-      </div>
+  
+  <div className="bg-animation">
+    <div id="stars"></div>
+    <div id="stars2"></div>
+  </div>
+
+  {/* <div className="background-svg">
+        <BackgroundLine/>
+    </div> */}
+    
+  <div className="relative w-full flex flex-col items-center h-full md:block">
+    {/* Decorative Images */}
+    <img src="/Ellipse 7.png" className="absolute left-[0px] top-[250px] hidden md:block" />
+    <img src="/Ellipse 8.png" className="absolute left-[200px] top-[600px] hidden md:block" />
+    <img src="/Ellipse 9.png" className="absolute right-[-30px] top-[450px] hidden md:block" />
+    
+    {/* Background Text */}
+    <div className='text-background-div1 hidden md:block'>
+      <p className='text-background'>Stay Updated</p>
     </div>
+
+    {/* Timeline Cards */}
+    <div className="md:absolute md:left-[185px] md:top-[180px] timeline-container1">
+      <TimeLineCard text={"Registrations Open"} month={"June"} day={"15"} />
+    </div>
+
+    <div className="md:absolute md:left-[885px] md:top-[387px] timeline-container">
+      <TimeLineCard text={"Registrations Open"} month={"June"} day={"15"} />
+    </div>
+
+    <div className="md:absolute md:left-[435px] md:top-[575px] timeline-container">
+      <TimeLineCard text={"Registrations Open"} month={"June"} day={"15"} />
+    </div>
+  </div>
+</div>
+
   )
 }
 
--- src/components/Timeline2.jsx
+++ src/components/Timeline2.jsx
@@ -1,21 +1,28 @@
 import React from "react"
 import TimeLineCard from "./TimeLineCard"
+import '../stylesheets/timeline.css';
 const Timeline = () => {
   return (
     <div className='relative h-screen w-full snap-start bg-gradient-to-b from-[#0F262A] via-[#0F262A] to-[#153f41] flex items-center justify-center text-4xl font-bold'>
-        <img src="/Ellipse 9.png" className="absolute right-[-30px] top-[-300px]"/>
-        <img src="/Ellipse 10.png" className="absolute right-[250px] top-[550px] h-36"/>
-        <div className='text-background-div2'>
+      <div className="bg-animation">
+        <div id="stars"></div>
+        <div id="stars2"></div>
+      </div>
+      <div className="relative w-full flex flex-col items-center h-full md:block">
+        <img src="/Ellipse 9.png" className="absolute right-[-30px] top-[-300px] hidden md:block"/>
+        <img src="/Ellipse 10.png" className="absolute right-[250px] top-[550px] h-36 hidden md:block"/>
+        <div className='text-background-div2 hidden md:block'>
           <p className='text-background'>Stay Updated</p>
         </div>
-      <div className="absolute left-[185px] top-[90px]">
+      <div className="md:absolute md:left-[185px] md:top-[90px] timeline-container1">
         <TimeLineCard text={"Registrations Open"} month={"June"} day={"15"} />
       </div>
-      <div className="absolute left-[755px] top-[227px]">
-        <TimeLineCard text={"Opening Registration"} month={"June"} day={"15"} />
+      <div className="md:absolute md:left-[755px] md:top-[227px] timeline-container">
+        <TimeLineCard text={"Registrations Open"} month={"June"} day={"15"} />
       </div>
-      <div className="absolute left-[485px] top-[487px]">
-        <TimeLineCard text={"Opening Registration"} month={"June"} day={"15"} />
+      <div className="md:absolute md:left-[485px] md:top-[487px] timeline-container">
+        <TimeLineCard text={"Registrations Open"} month={"June"} day={"15"} />
+      </div>
       </div>
     </div>
   )
