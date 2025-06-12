92d57d768bc304a4958a64974711be5af696c2e9
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-06-12 12:30:42 +0530

    chore: minor changes on timeline

--- src/components/TimeLineCard.jsx
+++ src/components/TimeLineCard.jsx
@@ -5,10 +5,10 @@ const TimeLineCard = ({ text, month, day }) => {
                 {text}
             </div>
             <div className='flex flex-col pt-4 pb-2 pr-6 items-end justify-center'>
-                <div className="text-[16px] lg:text-[28px] font-medium leading-[101%] tracking-[0.198px] capitalize bg-gradient-to-l from-[#0E9F9B] via-[#8FC18F] to-[#FFB34A] text-transparent bg-clip-text">
+                <div className="text-[14px] md:text-[16px] lg:text-[28px] font-medium leading-[101%] tracking-[0.198px] capitalize bg-gradient-to-l from-[#0E9F9B] via-[#8FC18F] to-[#FFB34A] text-transparent bg-clip-text">
                     {month}
                 </div>
-                <div className="text-[80px] lg:text-[120px] font-bold leading-[101%] tracking-[1.598px] capitalize bg-gradient-to-l from-[#0E9F9B] via-[#8FC18F] to-[#FFB34A] text-transparent bg-clip-text">
+                <div className="text-[60px] md:text-[80px] lg:text-[120px] font-bold leading-[101%] tracking-[1.598px] capitalize bg-gradient-to-l from-[#0E9F9B] via-[#8FC18F] to-[#FFB34A] text-transparent bg-clip-text">
                     {day}
                 </div>
             </div>
